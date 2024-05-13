7cd75924d8672f297094f1fcfac019f14b661ca7
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-13 19:10:26 +0100

    Added logo and select option for fonts.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [],
-//   theme: {
-//     extend: {},
-//   },
-//   plugins: [],
-// }
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   theme: {
     extend: {},
   },
