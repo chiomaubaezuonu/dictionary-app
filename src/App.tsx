d1dd47609a794274d012c026eb54ffcf71de1a89
Chioma Ubaezuonu <chiomaubaezuonu102@gmail.com>
2024-05-17 18:22:05 +0100

    displayed sourced urls from  the fetched data

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import moon from "./moon.png"
 import { Switch } from 'antd';
 import search from "./search.svg"
 import axios from "axios"
+import spinner from "./spinner.svg"
 
 
 function App() {
@@ -31,51 +32,66 @@ function App() {
   const [selectedFont, setSelectedFont] = useState<string>("sans-serif")
   const [audioData, setAudioData] = useState<string | null>(null);
   const [toggleOn, setToggleOn] = useState<boolean>(false)
+  const [isloading, setIsLoading] = useState<boolean>(false)
   //const [wordData, setWordData] = useState<Dictionary | null>(null)
 
 
 
   const onChange = (checked: boolean) => {
 
-    console.log(`switch to ${checked}`);
+    // console.log(`switch to ${checked}`);
     setToggleOn(checked)
   };
-  
+
 
   const audioRef = useRef<HTMLAudioElement | null>(null);
   useEffect(() => {
-    axios.get('https://api.dictionaryapi.dev/api/v2/entries/en/cat')
-      .then(response => {
-        console.log(response.data[0])
-        setDictionaryValue(response.data)
-        // setWordData(response.data[1])
-      })
-  }, [])
+    if (word.trim() !== "") {
+      setIsLoading(true)
 
-  // const handlePlay = () => {
-  //   alert("clicked")
-  //   if (audioRef.current) {
-  //     audioRef.current.play();
-  //   }
-  // };
+      axios.get(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+        .then(response => {
+          console.log(response.data[0])
+          setDictionaryValue(response.data)
+          // setWordData(response.data[1])
+        })
+        .catch(error => {
+          console.error("Fetching data failed:", error);
+        })
+        .finally(() => {
+          setIsLoading(false)
+        })
+    } else {
+      setDictionaryValue([])
+    }
+  }, [word])
 
-  const handleInput = (e: any) => {
+  const handlePlay = () => {
+    alert("clicked")
+    if (audioRef.current) {
+      audioRef.current.play();
+    }
+  };
+
+  const handleInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setWord(e.target.value)
   }
 
-
+  // max-w-3xl
   return (
-    <div className={`App max-w-3xl m-auto pt-16 px-[24px] ${toggleOn ? 'bg-black' : 'bg-white'}`}>
-      <header className=' max-w-3xl m-auto mt-16 px-[24px]'>
+    <div className={`App w-full  m-auto py-16 px-[24px] ${toggleOn ? 'bg-black' : 'bg-white'}`}>
+      <header className='max-w-3xl m-auto mt-16 px-[24px]'>
         <div className='flex justify-between'>
-          <img src={logo} alt='logo' className='w-11 h-11 object-cover' />
+          <div className='bg-black'>
+            <img src={logo} alt='logo' className='w-11 h-11 object-cover' />
+          </div>
           <div className='flex'>
             <div >
-              <select onChange={(e: any) => setSelectedFont(e.target.value)} id="" className='cursor-pointer border-y-0 border- border-r-2 p-2 pr-4 mr-4'>
-                <option value="sans-serif">Sans Serif</option>
-                <option value="serif">Serif</option>
-                <option value="mono">Mono</option>
+              <select onChange={(e: any) => setSelectedFont(e.target.value)} id="" className={`${toggleOn ? 'bg-black text-white' : 'bg-white'} cursor-pointer border-y-0 border- border-r-2 p-2 pr-4 mr-4`}>
+                <option className='cursor-pointer' value="sans-serif">Sans Serif</option>
+                <option className='cursor-pointer' value="serif">Serif</option>
+                <option className='cursor-pointer' value="mono">Mono</option>
               </select>
             </div>
             <div className="flex  ml-8 px-8 justify-between">
@@ -88,20 +104,14 @@ function App() {
         </div>
         <div>
           <span className='flex justify-between relative'>
-            <input onChange={handleInput} className={`py-5 px-6 ${toggleOn ? 'bg-[#1F1F1F]' : 'bg-[#F5F5F5]' } rounded-2xl text-xl font-bold mt-[4.5rem] w-full` } type="text" placeholder='Search for any word...' />
-            <img className='absolute top-24 cursor-pointer right-8 z-10 w-8' src={search} alt='search icon' />
+            <input onChange={handleInput} className={`py-5 px-6 ${toggleOn ? 'bg-[#1F1F1F] text-white' : 'bg-[#F5F5F5]'} rounded-2xl text-xl font-bold mt-[4.5rem] w-full`} type="text" placeholder='Search for any word...' />
+            <img onClick={() => handleInput} className='absolute top-24 cursor-pointer right-8 z-10 w-8' src={search} alt='search icon' />
           </span>
         </div>
       </header>
-      <div className='flex justify-between text-[#050505] items-center text-[4rem] font-bold py-6 px-6'>
-        <h1>{word}</h1>
-        {/* <img onClick={handlePlay} className='w-12 cursor-pointer' src={playAudio} alt="play audio icon" /> */}
 
-      </div>
-      <div>
-
-        {/* for displaying fetched objects */}
-        {/* {
+      {/* for displaying fetched objects */}
+      {/* {
   wordData && <div>
     <p>{wordData.phonetic}</p>
     <p>{wordData.meanings[0].partOfSpeech}</p>
@@ -109,24 +119,45 @@ function App() {
   </div>
 } */}
 
-
-
-        {dictionaryValue && dictionaryValue.map((item, index) => (
-          <div key={index} className={`font-${selectedFont}`}>
-            <p className='text-[#a445ed] mb-4 flex text-2xl place-items-start'>{item.phonetic}</p>
-            <p className=''>{item.meanings && item.meanings.map((each, meaningIndex) => (
-              <div key={meaningIndex}>
-                <h2 className='mb-6 flex font-bold justify-center text-[#050505] text-2xl italic my-10 items-center'>{each.partOfSpeech} <span className='h-[0.1rem] bg-gray-200 w-full ml-4'></span></h2>
-                <h2 className='flex place-items-start text-2xl'>Meaning</h2>
-                {each.definitions.map((definition) => (
-                  <li className='text-left marker:text-[#a445ed]'> {definition.definition}</li>
-                ))}
-                {/* <audio onClick={handlePlay} ref={audioRef} src={item.phonetics.audio} controls /> */}
-              </div>
-            ))}</p>
-
+      <div  className=' max-w-3xl m-auto mt-16 px-[24px]'>
+        {isloading ?
+          <div>
+            <img src={spinner} alt='spinner' />
           </div>
-        ))}
+          : (
+            <div>
+              <div className='flex justify-between text-[#050505] items-center text-[4rem] font-bold py-6 px-6'>
+                <h1 className={`${toggleOn ? 'text-white' : 'text-[#050505]'}`}>{word}</h1>
+                <img onClick={handlePlay} className='w-12 cursor-pointer' src={playAudio} alt="play audio icon" />
+
+              </div>
+              {dictionaryValue && dictionaryValue.map((item, index) => (
+                <div key={index} className={`font-${selectedFont}`}>
+                  <p className='text-[#a445ed] mb-4 flex text-2xl place-items-start'>{item.phonetic}</p>
+                  <p className=''>{item.meanings && item.meanings.map((each, meaningIndex) => (
+                    <div key={meaningIndex}>
+                      <h2 className={`mb-6 flex font-bold justify-center ${toggleOn ? 'text-white' : 'text-[#050505]'} text-2xl italic my-10 items-center`}>{each.partOfSpeech} <span className='h-[0.05rem] bg-gray-400 w-full ml-4'></span></h2>
+                      <h2 className='flex place-items-start text-2xl'>Meaning</h2>
+                      {each.definitions.map((definition) => (
+                        <div>
+                          <li className={` ${toggleOn ? 'text-white' : 'text-black'} py-2  text-left marker:text-[#a445ed]`}> {definition.definition}</li>
+
+                        </div>
+                      ))}
+                      {/* <audio onClick={handlePlay} ref={audioRef} src={item.phonetics.audio} controls /> */}
+                    </div>
+                  ))}</p>
+                 <div className={`${toggleOn ? 'text-[#F5F5F5]' : 'text-[#050505]'} flex flex-col place-items-start`}>
+                  <h4 className='my-4 '>Source</h4>
+                  <p className=''>{item.sourceUrls && item.sourceUrls[0]}</p>
+                 </div>
+                </div>
+
+              ))
+              }
+            </div>
+          )
+        }
       </div>
     </div >
   );
